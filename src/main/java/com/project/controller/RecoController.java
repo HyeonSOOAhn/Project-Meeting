7646fbb43293cbbae887a6a804476182ee036e89
HyeonSOOAhn <77277946+HyeonSOOAhn@users.noreply.github.com>
2021-06-24 17:13:41 +0900

    21/06/24 17/13 commit

---
+++ src/main/java/com/project/controller/RecoController.java
@@ -0,0 +1,84 @@
+package com.project.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.project.dao.RecoDAO;
+import com.project.dto.RecoDTO;
+
+@Controller
+public class RecoController {
+	
+	@Autowired
+	@Qualifier("recoDAO")
+	RecoDAO dao;
+	
+	
+	//메인
+	@RequestMapping(value = "/reco_main", method = RequestMethod.GET)
+	public String reco(Model model) throws IOException, InterruptedException {
+
+		return "reco/reco";
+	}
+	
+	//서브젝트별 리스트 불러오기
+	@RequestMapping(value = "/reco", method = RequestMethod.GET)
+	public String recoStudy(Model model,
+							@RequestParam(value = "searchKey", required = false) String search,
+							@RequestParam(value = "subject", required = true) String subject,	
+							@RequestParam(value = "pageNum", required = false, defaultValue = "1") int pageNum)
+									throws IOException, InterruptedException {
+		
+		int maxNum = dao.getMaxNum(subject);
+		List<RecoDTO> lists = dao.getLists(subject,pageNum);
+		
+		model.addAttribute("pageNum", pageNum);
+		model.addAttribute("maxNum", maxNum);
+		model.addAttribute("subject", subject);
+		model.addAttribute("lists", lists);
+		
+
+		return "reco/reco";
+	}
+	
+	
+	//자료 업데이트
+	@RequestMapping(value = "/reco_update", method = RequestMethod.GET)
+	public String recoUpdate() throws IOException, InterruptedException {
+		dao.insertData();
+
+		System.out.println("업데이트 완료");
+		
+		return "redirect:reco/reco_main";
+	}
+	
+	//하나의 데이터를 불러와서 그 정보와 지도를 호출
+	@RequestMapping(value = "/reco_showMap", method = RequestMethod.GET)
+	public String showMap(Model model,
+						  @RequestParam(value ="recoNum", required = true) int recoNum){
+		
+		RecoDTO list = dao.getReadData(recoNum);
+
+		model.addAttribute("list", list);
+
+		return "reco/showMap";
+
+	}
+	
+	@RequestMapping(value = "/reco_around", method = RequestMethod.GET)
+	public String around(Model model) {
+		
+		return "reco/reco";
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/project/parse/RecoDBStudyParser.java
@@ -0,0 +1,131 @@
+package com.project.parse;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.project.dto.RecoDTO;
+
+public class RecoDBStudyParser {
+	
+	public static int totalCount;//도서관 totalCount
+	
+	 
+	public static void totalCountParser() throws IOException, InterruptedException {
+    	//도서관 API의 totalCount를 가져온다.
+    	
+        StringBuilder urlBuilder = new StringBuilder("http://api.data.go.kr/openapi/tn_pubr_public_lbrry_api"); /*URL*/
+        urlBuilder.append("?" + URLEncoder.encode("ServiceKey","UTF-8") + "=wx1FtCvq2AivHsuIAfn24wTlffKB2K7uVzmPcNxwKSbT2ZNKATW4WdEDX%2Fx7MPv4PTxrP3zUqPANPq%2Byqdz5tg%3D%3D"); /*Service Key*/
+        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + 1); /*페이지 번호*/
+        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("100", "UTF-8")); /*한 페이지 결과 수*/
+        urlBuilder.append("&" + URLEncoder.encode("type","UTF-8") + "=" + URLEncoder.encode("json", "UTF-8")); /*XML/JSON 여부*/
+
+        URL url = new URL(urlBuilder.toString());
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-type", "application/json");
+        
+        BufferedReader rd;
+        if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+            rd = new BufferedReader(new InputStreamReader(conn.getInputStream(),"UTF-8"));
+        } else {
+            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+        }
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = rd.readLine()) != null) {
+            sb.append(line);
+        }
+        rd.close();
+        conn.disconnect();
+         
+        //파싱
+        JsonParser Parser = new JsonParser();
+        JsonObject jObj1 = (JsonObject) Parser.parse(sb.toString());
+        jObj1 = (JsonObject) jObj1.get("response");
+        jObj1 = (JsonObject) jObj1.get("body");
+        
+        totalCount = Integer.parseInt((((jObj1.get("totalCount")).toString()).replace("\"", "")));//데이터 전체 갯수
+        
+	}
+	
+    public static List<RecoDTO> libParser(int pageNo) throws IOException, InterruptedException {
+    	//도서관 API의 데이터를 dto에 담는다.
+        StringBuilder urlBuilder = new StringBuilder("http://api.data.go.kr/openapi/tn_pubr_public_lbrry_api"); /*URL*/
+        urlBuilder.append("?" + URLEncoder.encode("ServiceKey","UTF-8") + "=wx1FtCvq2AivHsuIAfn24wTlffKB2K7uVzmPcNxwKSbT2ZNKATW4WdEDX%2Fx7MPv4PTxrP3zUqPANPq%2Byqdz5tg%3D%3D"); /*Service Key*/
+        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + pageNo); /*페이지 번호*/
+        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("100", "UTF-8")); /*한 페이지 결과 수*/
+        urlBuilder.append("&" + URLEncoder.encode("type","UTF-8") + "=" + URLEncoder.encode("json", "UTF-8")); /*XML/JSON 여부*/
+
+        URL url = new URL(urlBuilder.toString());
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-type", "application/json");
+        
+        BufferedReader rd;
+        if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+            rd = new BufferedReader(new InputStreamReader(conn.getInputStream(),"UTF-8"));
+        } else {
+            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+        }
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = rd.readLine()) != null) {
+            sb.append(line);
+        }
+        rd.close();
+        conn.disconnect();
+         
+        //파싱
+        JsonParser Parser = new JsonParser();
+        JsonObject jObj1 = (JsonObject) Parser.parse(sb.toString());
+        jObj1 = (JsonObject) jObj1.get("response");
+        jObj1 = (JsonObject) jObj1.get("body");
+        JsonArray memberArray = (JsonArray) jObj1.get("items");
+        System.out.println(memberArray);
+        List<RecoDTO> list = new ArrayList<RecoDTO>();
+        
+        for (int i = 0; i < memberArray.size(); i++) {
+        	RecoDTO dto = new RecoDTO();
+        	JsonObject object = (JsonObject) memberArray.get(i);
+			
+			dto.setRecoNum(((pageNo-1)*100)+i);
+			dto.setSubject("study");
+			dto.setKeyword("library");
+        	dto.setTitle(((object.get("lbrryNm")).toString()).replace("\"", ""));//도서관명 title 
+        	dto.setIntroduce("시도명 : " + ((object.get("ctprvnNm")).toString()).replace("\"", "")//시도명 introduce
+        	+ "<br/>시군구명 : " + ((object.get("signguNm")).toString()).replace("\"", "")//시군구명 introduce
+        	+ "<br/>도서관 유형 : " + ((object.get("lbrrySe")).toString()).replace("\"", ""));//도서관 유형 introduce
+        	dto.setContent("휴관일 : " + ((object.get("closeDay")).toString()).replace("\"", "")//휴관일 content
+        	+ "<br/>평일운영시작시간 : " + ((object.get("weekdayOperOpenHhmm")).toString()).replace("\"", "")//평일운영시작시간 content
+        	+ "<br/>평일운영종료시간 : " + ((object.get("weekdayOperColseHhmm")).toString()).replace("\"", "")//평일운영종료시간 content
+        	+ "<br/>토요일운영시작시각 : " + ((object.get("satOperOperOpenHhmm")).toString()).replace("\"", "")//토요일운영시작시각 content
+        	+ "<br/>토요일운영종료시각 : " + ((object.get("satOperCloseHhmm")).toString()).replace("\"", "")//토요일운영종료시각 content
+        	+ "<br/>공휴일운영시작시각 : " + ((object.get("holidayOperOpenHhmm")).toString()).replace("\"", "")//공휴일운영시작시각 content
+        	+ "<br/>공휴일운영종료시각 : " + ((object.get("holidayCloseOpenHhmm")).toString()).replace("\"", "")//공휴일운영종료시각 content
+        	+ "<br/>도서관전화번호 : " + ((object.get("phoneNumber")).toString()).replace("\"", ""));////도서관전화번호 content
+        	dto.setLocation("소재지도로명주소 : " + ((object.get("rdnmadr")).toString()).replace("\"", ""));//소재지도로명주소 location
+        	dto.setLat(((object.get("latitude")).toString()).replace("\"", ""));
+        	dto.setLon(((object.get("longitude")).toString()).replace("\"", ""));
+        	
+        	list.add(dto);
+        	
+
+        }
+        
+        
+        System.out.println(pageNo);
+        return list;
+        
+        
+		
+    }
+}
